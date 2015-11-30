010440eb7b4532a7876cedb0d580d1bdb2e3a390
crashtestzombie0@gmail.com <crashtestzombie0@gmail.com>
2015-11-30 15:18:14 -0500

    created & scaffolded recipe branch; migrated database

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151130195946) do
+ActiveRecord::Schema.define(version: 20151130201300) do
 
   create_table "cookbooks", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "recipes", force: :cascade do |t|
+    t.string   "name"
+    t.string   "category"
+    t.string   "cuisine"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
